d46346befd720c684e8f07613af92235b68dcad2
Will hunt <willhunt@Wills-MacBook-Pro.local>
2026-05-09 20:56:20 -0700

    clean dead

--- src/App.js
+++ src/App.js
@@ -1,51 +1,20 @@
 import './App.css';
 import { HashRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import { AnimatePresence } from 'framer-motion';
+// eslint-disable-next-line no-unused-vars
 import CleanHome from './pages/CleanHome';
-import Projects from './pages/Projects';
-import Favorites from './pages/Favorites';
-import FavoritesYear from './pages/FavoritesYear';
-import Footer from './components/Footer';
+import Home from './pages/Home';
 
 function App() {
   return (
     <HashRouter>
-      <AnimatePresence mode="wait">
-        <Routes>
-          <Route path="/" element={
-            <div className='min-h-screen flex flex-col'>
-              <CleanHome />
-            </div>
-          } />
-          <Route path="/projects" element={
-            <div className='min-h-screen flex flex-col cursor-auto'>
-              <Navbar />
-              <main className='flex-1'>
-                <Projects />
-              </main>
-              <Footer />
-            </div>
-          } />
-          <Route path="/favorites" element={
-            <div className='min-h-screen flex flex-col cursor-auto hide-scrollbar'>
-              <Navbar />
-              <main className='flex-1'>
-                <Favorites />
-              </main>
-              <Footer />
-            </div>
-          } />
-          <Route path="/favorites/:year" element={
-            <div className='min-h-screen flex flex-col cursor-auto hide-scrollbar'>
-              <Navbar />
-              <main className='flex-1'>
-                <FavoritesYear />
-              </main>
-            </div>
-          } />
-        </Routes>
-      </AnimatePresence>
+      <Routes>
+        <Route path="/" element={
+          <div className='min-h-screen flex flex-col'>
+            {/* <CleanHome /> */}
+            <Home />
+          </div>
+        } />
+      </Routes>
     </HashRouter>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+function Home() {
+    return (
+        <div style={{
+            flex: 1, 
+            backgroundColor: 'red',
+            boxSizing: "border-box",
+            border: 20,
+            borderColor: 'blue'
+        }}>
+                
+        </div>
+    )
+}
+
+export default Home;
